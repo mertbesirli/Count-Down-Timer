7ce876f885cb2884eda07ec2148865bf25c7f86e
mertbesirli <43350594+mertbesirli@users.noreply.github.com>
2022-07-14 18:47:06 +0300

    A starter mini-project for JavaScript on the web.

---
+++ src/script.js
@@ -0,0 +1,34 @@
+
+const daysEl = document.getElementById("days");
+const hoursEl = document.getElementById("hours");
+const minsEl = document.getElementById("mins");
+const secondsEl = document.getElementById("seconds");
+
+const dates = "1 Jan 2023"
+
+function arrangeDate(){
+
+    const newDate = new Date(dates)
+
+    const currentDate = new Date()
+
+    const totalSeconds = (newDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const mins = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formatTime(hours);
+    minsEl.innerHTML = formatTime(mins);
+    secondsEl.innerHTML = formatTime(seconds);
+}
+
+function formatTime(time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+arrangeDate()
+
+setInterval(arrangeDate, 1000)
